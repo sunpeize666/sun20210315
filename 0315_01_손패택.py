6f53e8e340291c2d01e5dd61a5ba1a18da123b7e
sunpeize666 <144121626+sunpeize666@users.noreply.github.com>
2024-03-15 15:03:29 +0900

    Add files via upload

---
+++ 0315_01_손패택.py
@@ -0,0 +1,26 @@
+'''
+2024.03.15
+컴퓨터학과 202195057 손패택
+'''
+#10을 변수 사용과 출럭 함수 사용법
+num = 10
+
+pi = 3.14
+
+#자기 이름을 변수에 저장하시오.
+name = '손패택'
+
+#각 변수에 저장된 값을 출럭하시오.
+
+print("num에 저장된 값 : ", num)
+print("num에 저장된 값은 {}입니다." .format(num))
+
+print("pi에 저장된 값 : ", pi)
+print("pi에 저장된 값 :  {}" .format(pi))
+
+print("제 이름은 ", name, "입나다.")
+print("제 이름음 {}입니다." . format(name))
+
+print("num의 자료형 : ", type(num))
+print("pi 지료형 : ", type(pi))
+print("name의 자료형")
